50364936c30e5cbd522d9dfd307b99d6de97fc41
Maya Man <mayaman26@gmail.com>
2020-09-04 12:14:23 -0400

    new css only marquee animation

--- index.js
+++ index.js
@@ -82,31 +82,3 @@ function toggleView() {
     homeContainer.style.display = 'block';
   }
 }
-
-
-/*
- * Start marquee tag effect
- */
-window.addEventListener('load', function () {
-  function go() {
-    i = i < width ? i + step : 1;
-    m.style.marginLeft = -i + 'px';
-  }
-  let i = 0,
-    step = 3,
-    space = ''; // 
-  let m = document.getElementById('marquee');
-  let t = m.innerHTML; //text
-  m.innerHTML = t + space;
-  m.style.position = 'absolute'; // http://stackoverflow.com/questions/2057682/determine-pixel-length-of-string-in-javascript-jquery/2057789#2057789
-  let width = (m.clientWidth + 1);
-  m.style.position = '';
-  m.innerHTML = t + space + t + space + t + space + t + space + t + space + t + space + t + space;
-  m.addEventListener('mouseenter', function () {
-    step = 0;
-  }, true);
-  m.addEventListener('mouseleave', function () {
-    step = 3;
-  }, true);
-  let x = setInterval(go, 50);
-}, true);
